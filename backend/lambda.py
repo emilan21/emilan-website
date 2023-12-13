ca660044866ba557567d75c93525020be4d9036c
Eric Milan <emilan@ericmilan.dev>
2023-12-12 19:49:06 -0500

    Trying stuff out

--- backend/lambda.py
+++ backend/lambda.py
@@ -5,7 +5,13 @@ table = dynamodb.Table('ViewCounts')
 
 
 def lambda_handler(event, context):
-    response = table.put_item(Item={
-        'counts': 4,
-    })
-    return response
+    # Get current counts
+    response = table.get_item(Key={'counts'})
+    print(response['Item'])
+
+    #new_count = response['Item'] + 1
+    #response = table.put_item(Item={
+    #    'counts': new_count,
+    #})
+    #return response
+    return 0
