20c00957f8431f6da9f7a458a50975261c80de58
Eric Milan <emilan@ericmilan.dev>
2023-12-14 17:27:46 -0500

    Split into two lambda handlers. One for getting the current item. One for incrementing the count of an item by one.

---
+++ backend/increment_visit_count.py
@@ -0,0 +1,27 @@
+import os
+import boto3
+
+
+def lambda_handler(event, context):
+    event_id = event["id"]
+
+    # Create a Dynamodb client
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ["TABLE_NAME"]
+    table = dynamodb.Table(table_name)
+
+    # Get the current visit count if it exist
+    response = table.get_item(Key={"id": event_id})
+    if "Item" in response:
+        visit_count = response["Item"]["count"]
+
+    # Increment the number of visits by 1.
+    visit_count += 1
+
+    # Put the new visit count into the table.
+    response = table.put_item(Item={"id": event_id, "count": visit_count})
+
+    return {
+        "statusCode": response['ResponseMetadata']['HTTPStatusCode'],
+        'body': 'Record ' + event_id + ' added. Count is now ' + visit_count
+    }
